56b2cd116b1202870c9db94c4b1be417dde57b47
wangyaoqianw-max <wangyaoqianw@gmail.com>
2026-09-02 10:03:11 +0800

    修正软件I2C引脚模式选择

--- RTT_elog_DMA_UART_ring_project/Core/Src/gpio.c
+++ RTT_elog_DMA_UART_ring_project/Core/Src/gpio.c
@@ -71,7 +71,7 @@ void MX_GPIO_Init(void)
 
   /*Configure GPIO pins : PBPin PBPin */
   GPIO_InitStruct.Pin = I2C_SCL_Pin|I2C_SDA_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
